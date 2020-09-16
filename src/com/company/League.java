b3ed9af9e073ded3926b6429764093cfbde22164
Ralph F <RaftsDev@gmail.com>
2020-09-16 12:17:05 -0400

    Feat: Create class League.

---
+++ src/com/company/League.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class League {
+    private String name;
+    Arraylist<Team> team = new ArrayList<Team>();
+
+    public League(String name) {
+        this.name = name;
+    }
+}
