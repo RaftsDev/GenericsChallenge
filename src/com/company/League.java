2e106b89d4e05b80d562f145195aec73b474634b
Ralph F <RaftsDev@gmail.com>
2020-09-17 08:32:45 -0400

    Feat: Add showTable() in League class.

--- src/com/company/League.java
+++ src/com/company/League.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 
 public class League {
     private String name;
-    ArrayList<Team> team = new ArrayList<Team>();
+    ArrayList<Team> league = new ArrayList<Team>();
 
     public League(String name) {
         this.name = name;
+    }
+
+    public void showTable() {
+        for( Team i: this.league) {
+            System.out.println("League:"+i.getName());
+        }
     }
 }
--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -6,4 +6,8 @@ public class Team {
     public Team(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
 }
