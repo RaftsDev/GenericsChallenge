39616aa4062874602a66b742a173b633f71e161f
Ralph F <RaftsDev@gmail.com>
2020-09-17 08:39:36 -0400

    Feat: Add teams in Main class.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        BasketBallTeam chBulls = new BasketBallTeam("Ch Bulls");
+        BasketBallTeam nycBulls = new BasketBallTeam("NYC Bulls");
+
+        HockeyTeam pitPing = new HockeyTeam("Pit Ping");
+        HockeyTeam atlanticPing = new HockeyTeam("Atlantic Ping");
+
 
     }
 }
